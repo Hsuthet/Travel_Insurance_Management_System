f3f46d9cbd8b48b6bfe3154e9634ee81d3ee7720
hninyu <hninyuga@gmail.com>
2026-05-04 15:01:00 +0630

    .

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 // This tells Laravel to look for your command and run it every day at midnight
-Schedule::command('app:close-expired-contracts')->daily();
+Schedule::command('app:close-expired-contracts')->everyMinute();
